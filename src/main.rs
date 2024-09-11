0901293b39977fb55370182d80187fa104797fac
Lyrimi <lyrimisin@gmail.com>
2024-09-11 10:33:22 +0200

    added rng gen

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,17 @@
 use enigo::{
     Enigo, Keyboard, Settings,
 };
-
-
+use std::{thread, time};
+use rand::{self, Rng};
 
 fn main() {
-    let mut enigo = Enigo::new(&Settings::default()).unwrap();
-    let resault = enigo.text("hello world!");
-    println!("{:?}", resault);
+    let mut rng = rand::thread_rng();
+    loop {
+        thread::sleep(time::Duration::from_millis(1000));
+        if rng.gen_range(1..=10) == 1 {
+            let mut enigo = Enigo::new(&Settings::default()).unwrap();
+            let resault = enigo.text("hello world!");
+            println!("Wrote stuff{:?}", resault);
+        } 
+    }
 }
